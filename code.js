c9ef5d16535b64b72ea186bcb157de7cc9972946
HectorVilas <96928935+HectorVilas@users.noreply.github.com>
2022-07-20 15:56:24 -0300

    function to swap side

--- code.js
+++ code.js
@@ -5,12 +5,13 @@ const
   bgLogin = document.querySelector(".bg-login"),
   bgSignUp = document.querySelector(".bg-signup"),
   btnsDiv = document.querySelector(".buttons"),
-  btnLogin = document.querySelector(".login"),
-  btnSignUp = document.querySelector(".signup"),
+  btnLogin = document.querySelector(".main .buttons .login"),
+  btnSignUp = document.querySelector(".main .buttons .signup"),
   //forms
   menuLogin = document.querySelector(".menu-login"),
   menuSignUp = document.querySelector(".menu-signup"),
-  inputs = document.querySelectorAll("input");
+  inputs = document.querySelectorAll("input"),
+  swapSide = document.querySelectorAll(".swap-side");
 
 
 
@@ -20,11 +21,16 @@ btnSignUp.addEventListener("click", moveMenu);
 inputs.forEach(input => {
   input.addEventListener("focusout", () => {
     input.classList.add("visited")
-  });
-})
+  })
+});
+swapSide.forEach(a => {
+  a.addEventListener("click", swap)
+});
 
 
 //functions
+
+//moves the menu left or right to reveal the proper form, becoming a sidebar
 function moveMenu(){
   btnLogin.removeEventListener("click", moveMenu);
   btnSignUp.removeEventListener("click", moveMenu);
@@ -46,8 +52,32 @@ function moveMenu(){
     loginSignUp.style.left = "0";
     loginSignUp.style.right = "auto";
   }
-}
+};
 
+//swap sides with "already have account?"/"need an account?"
+function swap(){
+  const hidden = document.querySelector(".hide-side"),
+    smaller = document.querySelector(".smaller-side");
+  
+  hidden.classList.remove("hide-side");
+  smaller.classList.remove("smaller-side");
+  
+  setTimeout(() => {
+    if(menuLogin.className.includes("hidden")){
+      menuLogin.classList.remove("hidden")
+      menuSignUp.classList.add("hidden")
+      loginSignUp.style.right = "0";
+      loginSignUp.style.left = "auto";
+    } else {
+      menuSignUp.classList.remove("hidden")
+      menuLogin.classList.add("hidden")
+      loginSignUp.style.left = "0";
+      loginSignUp.style.right = "auto";
+    };
+    hidden.classList.add("smaller-side")
+    smaller.classList.add("hide-side")
+  }, 2000);
+};
 
 
 //this is for testing
